d4d1045c7700d2c538e7e9aa53ea5aff2f7918b7
piotrkardys <piotrk@mobilewebpro.pl>
2016-04-13 16:00:30 +0200

    Lesson 11 - refreshing of the single content only (not all page)

--- Profile.js
+++ Profile.js
@@ -19,22 +19,31 @@ var Profile = React.createClass ({
   componentDidMount: function() {
     //this.ref = new Firebase('https://github-note-taker.firebaseio.com/');
     this.ref = new Firebase('https://resplendent-fire-4532.firebaseio.com/');
-    var childRef = this.ref.child(this.props.params.username);
-    this.bindAsArray(childRef, 'notes');                        //addListener to notes table
-
-    helpers.getGithubInfo(this.props.params.username).then(function(data) {
-      this.setState({
-        bio: data.bio,
-        repos: data.repos
-      })
-    }.bind(this))
+    this.init(this.props.params.username);
+  },
+  componentWillReceiveProps: function(nextProps) {      //if executed - change the content on the page
+    this.unbind('notes');                               //unbind notes (from the DB) firstly
+    this.init(nextProps.params.username);               //calls this function for changing that content
   },
   componentWillUnmount: function() {
     this.unbind('notes');                                       //removeListener from notes table
   },
+
+  init: function(username) {
+    var childRef = this.ref.child(username);
+    this.bindAsArray(childRef, 'notes');                        //addListener to notes table
+
+    helpers.getGithubInfo(username).then(function(data) {   //calls the method getGithubInfo with parameters
+      this.setState({                                                         //results of that function are written in the variable
+        bio: data.bio,                                                        //and method setState is called (to sets fields properly)
+        repos: data.repos
+      })
+    }.bind(this))                                                             //that is because of calling 'this' properly (class Profile, not object helpers)
+  },
   handleAddNote: function(newNote) {        //function adds new note to the end of the notes' list
     this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote)
   },
+  
   render: function() {
                                 //this.props.params - parameters via Router
     return (                    //calling components with parameters
